0bf8352994f88b3e8c6585b6917a350ead47eb92
Usama Navid <concaption@gmail.com>
2023-05-20 19:34:10 +0000

    adding python initial scafold

---
+++ hello.py
@@ -0,0 +1,15 @@
+def more_hello():
+    return "hi"
+
+
+def more_goodbye():
+    return "bye"
+
+
+# this is a nonsense code that generate linting error
+var = 1
+var = var
+
+# Bad syntax
+
+# foo =
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import more_hello
+
+
+def test_more_hello():
+    assert more_hello() == "hi"
